4fdef67733bc5defe1d79b34dc2c79ec93bd8959
KeKilledYou <95946578+KeKilledYou@users.noreply.github.com>
2025-08-21 14:07:06 -0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,14 +1,13 @@
-shared_script '@WaveShield/resource/waveshield.lua' --this line was automatically written by WaveShield
-
 fx_version 'cerulean'
 game 'gta5'
-lua54 'yes'  -- <--- THIS ENABLES LUA 5.4
+lua54 'yes' 
 
 author 'Almighty Ke'
-description 'Photo request system'
-version '1.0.0'
+description 'Photo request'
+version '1.0'
 
 shared_script '@ox_lib/init.lua'
 
 client_script 'client.lua'
 server_script 'server.lua'
+
